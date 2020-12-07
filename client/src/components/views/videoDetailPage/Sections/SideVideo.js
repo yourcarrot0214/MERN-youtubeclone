78e9bdb3ac618b507b8d823f3b0de9a96a252501
your Carrot <stylenbs@gmail.com>
2020-12-07 19:45:17 +0900

    #10 Side Video set up

---
+++ client/src/components/views/videoDetailPage/Sections/SideVideo.js
@@ -0,0 +1,62 @@
+import React, { useState, useEffect } from "react";
+import Axios from "axios";
+
+function SideVideo() {
+  const [sideVideos, setSideVideos] = useState([]);
+
+  useEffect(() => {
+    Axios.get("/api/video/getVideos").then((response) => {
+      if (response.data.success) {
+        console.log(response.data);
+        setSideVideos(response.data.videos);
+        console.log(sideVideos);
+      } else {
+        console.log("Failed :: get Videos.");
+      }
+    });
+  }, []);
+
+  const renderSideVideo = sideVideos.map((video, index) => {
+    return (
+      <div
+        key={index}
+        style={{ display: "flex", marginBottom: "1rem", padding: "0 2rem" }}
+      >
+        <div
+          style={{ width: "40%", marginRight: "1rem", marginBottom: "1rem" }}
+        >
+          <a href={`/video/${video._id}`}>
+            <img
+              style={{ width: "100%" }}
+              src={`http://localhost:5000/${video.thumbnail}`}
+              alt="thumbnail"
+            />
+          </a>
+        </div>
+
+        <div style={{ width: "50%" }}>
+          <a href={`/video/${video._id}`}>
+            <span style={{ fontSize: "1rem", color: "black" }}>
+              {video.title}
+            </span>
+            <br />
+            <span>{video.writer.name}</span>
+            <br />
+            <span>{video.views} views</span>
+            <br />
+            <span>{Math.floor(video.duration)} seconds</span>
+            <br />
+          </a>
+        </div>
+      </div>
+    );
+  });
+
+  return (
+    <React.Fragment>
+      <div style={{ marginTop: "3rem" }}>{renderSideVideo}</div>
+    </React.Fragment>
+  );
+}
+
+export default SideVideo;
--- client/src/components/views/videoDetailPage/VideoDetailPage.js
+++ client/src/components/views/videoDetailPage/VideoDetailPage.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { Row, Col, List, Avatar } from "antd";
 import Axios from "axios";
+import SideVideo from "./Sections/SideVideo";
 
 function VideoDetailPage(props) {
   const videoId = props.match.params.videoId;
@@ -43,7 +44,7 @@ function VideoDetailPage(props) {
         </Col>
 
         <Col lg={6} xs={24}>
-          Side Videos
+          <SideVideo />
         </Col>
       </Row>
     );
